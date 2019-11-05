c7574dfcd66db5f4749f6c47e2cb71d59f75ad50
Hugo Ladeira de Freitas <HUGO_LADEIRA@hotmail.com>
2019-11-04 21:45:51 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv/config');
+
 module.exports = {
   dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
