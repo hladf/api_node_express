3e9f369659ad2519e75a1b3f7ce9e48c909a5dd5
Hugo Ladeira de Freitas <HUGO_LADEIRA@hotmail.com>
2019-11-19 13:54:01 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -25,6 +26,8 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    // this.server.use(cors({ origin: 'https://www.exemplo.com.br' }));
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
