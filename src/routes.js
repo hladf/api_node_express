727bdc9f293992d481e79516b1216ebb567915c5
Hugo Ladeira de Freitas <HUGO_LADEIRA@hotmail.com>
2019-11-18 23:07:16 -0300

    add route validators

--- src/routes.js
+++ src/routes.js
@@ -11,33 +11,55 @@ import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
 import AvailableController from './app/controllers/AvailableController';
 
-import validateUserStore from './app/validators/UserStore';
-import validateUserUpdate from './app/validators/UserUpdate';
+import UserStoreValidator from './app/validators/UserStoreValidator';
+import UserUpdateValidator from './app/validators/UserUpdateValidator';
+import SessionStoreValidator from './app/validators/SessionStoreValidator';
+import AppointmentStoreValidator from './app/validators/AppointmentStoreValidator';
 
 import authMiddleware from './app/midlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('/users', validateUserStore, UserController.store);
-routes.post('/sessions', SessionController.store);
+// User/Session:
+routes.post('/users', UserStoreValidator, UserController.store);
+routes.post('/sessions', SessionStoreValidator, SessionController.store);
 
+// tudo após essa linha vai passar pelo authMiddleware.
 routes.use(authMiddleware);
 
-routes.put('/users', validateUserUpdate, UserController.update);
+routes.put('/users', UserUpdateValidator, UserController.update);
+/********************/
 
+// Providers:
 routes.get('/providers', ProviderController.index);
+/********************/
+
+// Availables:
 routes.get('/providers/:providerId/available', AvailableController.index);
+/********************/
 
+// Appointments:
 routes.get('/appointments', AppointmentController.index);
-routes.post('/appointments', AppointmentController.store);
+routes.post(
+  '/appointments',
+  AppointmentStoreValidator,
+  AppointmentController.store
+);
 routes.delete('/appointments/:id', AppointmentController.delete);
+/********************/
 
+// Schedules:
 routes.get('/schedule', ScheduleController.index);
+/********************/
 
+// Notifications:
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
+/********************/
 
+// Files:
 routes.post('/files', upload.single('file'), FileController.store);
+/********************/
 
 export default routes;
