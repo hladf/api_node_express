184bcd66ef53913056e31d64c1077f1c179419d8
Hugo Ladeira de Freitas <HUGO_LADEIRA@hotmail.com>
2019-11-19 20:52:09 -0300

    add AvailableService

---
+++ src/app/services/AvailableService.js
@@ -0,0 +1,63 @@
+import {
+  setSeconds,
+  setMinutes,
+  setHours,
+  isAfter,
+  format,
+  startOfDay,
+  endOfDay,
+} from 'date-fns';
+import { Op } from 'sequelize';
+
+import Appointment from '../models/Appointment';
+
+class AvailableService {
+  async list({ searchDate, provider_id }) {
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+        },
+      },
+    });
+
+    // passar isso pra uma tabela no banco onde o próprio provider
+    // cadastra seu horario de trabalho e agendamentos
+    const schedule = [
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+    ];
+
+    const available = schedule.map(time => {
+      const [hour, minute] = time.split(':');
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        available:
+          isAfter(value, new Date()) &&
+          !appointments.find(a => format(a.date, 'HH:mm') === time),
+      };
+    });
+
+    return available;
+  }
+}
+
+export default new AvailableService();
